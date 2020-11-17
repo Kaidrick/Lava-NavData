d61a5810594e0cf7692aaeb01a038052185d4e00
Kaidrick <cjt422@gmail.com>
2020-11-18 00:21:07 +0800

    added vue template for new web gui

---
+++ web/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+import i18n from './i18n'
+
+Vue.config.productionTip = false;
+
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : '/api';
+
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
+
+new Vue({
+  router,
+  i18n,
+  render: h => h(App)
+}).$mount('#app')
---
+++ web/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  publicPath: './',
+
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+    }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
